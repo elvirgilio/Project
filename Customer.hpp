907e263a7d8eb443fdf53f01191d8dc12d84187a
abdelmaksou <ibrahimabdelmaksoud22@gmail.com>
2021-04-27 02:36:27 +0200

    CustomerProfile, Auth, Customer header files

---
+++ Customer.hpp
@@ -0,0 +1,24 @@
+#ifndef CUSTOMER_hpp
+#define CUSTOMER_hpp
+
+#include "CustomerProfile.hpp"
+#include "Wallet.hpp"
+#include "CreditCard.hpp"
+#include "Reward.hpp"
+#include "Reservation.hpp"
+
+Class Customer
+{
+private:
+	Wallet wallet;
+	CreditCard creditCard;
+	vector <Reservation> reservations;
+	Reward reward;
+	CustomerProfile Customer_Profile;
+public:
+	void add_res(Reservation r);
+	Reservation *get_res(int i);
+	void cancelRes(int i);
+};
+#endif
+
