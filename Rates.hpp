3548662be3a0a8ce024a45b3a91a337ac0babde3
abdelmaksou <ibrahimabdelmaksoud22@gmail.com>
2021-04-27 03:08:27 +0200

    rates header file

---
+++ Rates.hpp
@@ -0,0 +1,15 @@
+#ifndef RATES_hpp
+#define RATES_hpp
+
+#include "Reservation.hpp"
+
+Class Rates
+{
+private:
+	Reservation reservation;
+public:
+	void rateHotel();
+	void rateTrip();
+};
+
+#endif
